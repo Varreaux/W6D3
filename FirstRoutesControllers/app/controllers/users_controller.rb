e52b4d8cc2f43a1e25b6756aab4b0a95263649de
Yong <yong.lin13@gmail>
2023-02-16 10:34:54 -0500

    added some users

--- FirstRoutesControllers/app/controllers/users_controller.rb
+++ FirstRoutesControllers/app/controllers/users_controller.rb
@@ -2,13 +2,22 @@
 
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
     end
-
 
     def create
-        render json: params #['some_category']
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+          render json: user
+        else
+          render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
+
+    # def create
+    #     render json: params #['some_category']
+    # end
 
     def show
         render json: params
