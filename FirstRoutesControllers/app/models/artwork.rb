38c191ee07ac39688f99ccf43ab9eb9b213bbd9d
Yong <yong.lin13@gmail>
2023-02-16 15:18:02 -0500

    completed artwork_share

---
+++ FirstRoutesControllers/app/models/artwork.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artwork < ApplicationRecord
+    validates :title, presence: true
+    validates :image_url, :artist_id, presence: true, uniqueness: true
+    validates :title, uniqueness: {scope: :artist_id, message: "should be unique per artist"}
+
+    belongs_to :artist,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :User
+
+    has_many :artwork_shares,
+        primary_key: :id,
+        foreign_key: :artwork_id,
+        class_name: :ArtworkShare
+end
